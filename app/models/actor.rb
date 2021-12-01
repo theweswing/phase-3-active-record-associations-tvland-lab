6c4ffbb85a313f152d10099f3c8db6eca93c3998
Wes Schierenbeck <wschierenbeck@gmail.com>
2021-12-01 14:23:02 -0500

    commit tree 33a514f37e19c80dc392bbed3704211ce29902f4

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    characters = self.characters
+    all_roles =
+      characters.map do |given_character|
+        "#{given_character.name} - #{given_character.show.name}"
+      end
+    all_roles
+  end
 end
